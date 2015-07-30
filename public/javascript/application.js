0aa20fddf98248f0e84c5c50c667f7f22664ed96
Sonny Sekhon <sonnysekhon@gmail.com>
2015-07-30 01:00:36 +0000

    Authentication Added

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,19 @@
-$(document).ready(function() {
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+$(function() {
+  $("#search").on("submit", function (evt){
+    evt.preventDefault();
+    $("#results").empty();
+    var searchInput = $("#search-input").val();
+    $.ajax({
+      type: "GET",
+      dataType: "jsonp",
+      url: "https://api.instagram.com/v1/tags/" + searchInput + "/media/recent?client_id=e99ade1f77474c24b089c7cb2b43f7a6",
+      success: function(obj) {
+        obj.data.forEach(function(result) {
+          var image = result.images.thumbnail.url;
+          var caption = result.caption.text;
+          $("#results").append("<img src='" + image + "' />"); 
+        });
+      }
+    });
+  });
 });
